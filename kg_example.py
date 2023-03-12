7c5410094588ace97c332b11f165e3ff37def266
pleGGen <crelative@163.com>
2023-03-12 15:50:51 +0800

    update

--- kg_example.py
+++ kg_example.py
@@ -2,8 +2,10 @@
 from neo4j import GraphDatabase
 import logging
 from neo4j.exceptions import ServiceUnavailable
+import json
 ## 安装完别忘了添加以下下面 todo 的参数
 ## 比较隐私的内容请做好保护
+
 
 class App:
 
@@ -14,49 +16,115 @@ class App:
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_friendship(self, person1_name, person2_name):
+    # 创建节点
+    def create_node(self, node_data):
         with self.driver.session(database="neo4j") as session:
             # Write transactions allow the driver to handle retries and transient errors
-            result = session.execute_write(
-                self._create_and_return_friendship, person1_name, person2_name)
+            result = ""
+            if( 'alias' in node_data['content']):
+                result = session.execute_write(
+                    self._create_node_with_alias, node_data['content']['type'], node_data['name'], node_data['content']['alias']
+                )
+            else:
+                result = session.execute_write(
+                    self._create_node_without_alias, node_data['content']['type'], node_data['name']
+                )
+            
+            assert(result != "")
             for row in result:
-                print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
+                print("Created node: {node_name}".format(node_name=row['name']))
 
     @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
+    def _create_node_with_alias(tx, node_type,node_name,node_alias):
         # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
         # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
+        query = ()
+        match node_type:
+            case 'disease':
+                query = (
+                    "CREATE (p1: Disease { name: $node_name, alias: $node_alias }) "
+                    "RETURN p1"
+                )
+            case 'chemical':
+                query = (
+                    "CREATE (p1: Chemical { name: $node_name, alias: $node_alias }) "
+                    "RETURN p1"
+                )
+            case 'genetic':
+                query = (
+                    "CREATE (p1: Genetic { name: $node_name, alias: $node_alias }) "
+                    "RETURN p1"
+                )
+            case 'treatment':
+                query = (
+                    "CREATE (p1: Treatment { name: $node_name, alias: $node_alias }) "
+                    "RETURN p1"
+                )
+        assert(query != ())
+        result = tx.run(query,node_name=node_name,node_alias=node_alias)
         try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]}
+            return [{"name": row["p1"]["name"]}
                     for row in result]
         # Capture any errors along with the query and data for traceability
         except ServiceUnavailable as exception:
             logging.error("{query} raised an error: \n {exception}".format(
                 query=query, exception=exception))
             raise
-
-    def find_person(self, person_name):
+    
+    @staticmethod
+    def _create_node_without_alias(tx, node_type,node_name):
+        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
+        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
+        query = ()
+        match node_type:
+            case 'disease':
+                query = (
+                    "CREATE (p1: Disease { name: $node_name }) "
+                    "RETURN p1"
+                )
+            case 'chemical':
+                query = (
+                    "CREATE (p1: Chemical { name: $node_name }) "
+                    "RETURN p1"
+                )
+            case 'genetic':
+                query = (
+                    "CREATE (p1: Genetic { name: $node_name }) "
+                    "RETURN p1"
+                )
+            case 'treatment':
+                query = (
+                    "CREATE (p1: Treatment { name: $node_name }) "
+                    "RETURN p1"
+                )
+        assert(query != ())
+        result = tx.run(query,node_name=node_name)
+        try:
+            return [{"name": row["p1"]["name"]}
+                    for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+    
+    def find_node(self, node_data):
         with self.driver.session(database="neo4j") as session:
-            result = session.execute_read(self._find_and_return_person, person_name)
+            result = session.execute_read(self._find_and_return_person, node_data['content']['type'], node_data['name'])
             for row in result:
                 print("Found person: {row}".format(row=row))
-
+    #todo 要修改 node_type 不能为query参数 
     @staticmethod
-    def _find_and_return_person(tx, person_name):
+    def _find_and_return_node(tx, node_type, node_name):
+        # 执行语句
         query = (
-            "MATCH (p:Person) "
-            "WHERE p.name = $person_name "
+            "MATCH (p: $node_type) "
+            "WHERE p.name = $node_name "
             "RETURN p.name AS name"
         )
-        result = tx.run(query, person_name=person_name)
+        result = tx.run(query, node_type=node_type, node_name=node_name)
         return [row["name"] for row in result]
+
 
 
 if __name__ == "__main__":
@@ -67,6 +135,17 @@ if __name__ == "__main__":
     password = "..." # the password you set
     
     app = App(uri, user, password)
-    app.create_friendship("Alice", "David")
-    app.find_person("Alice")
+    with open("./ner_output/entities.json","r") as f:
+        data = json.load(f)
+
+    entity = {
+        'name':"",
+        'content':{}
+    }
+
+    for key, value in data.items():
+        entity['name'] = key
+        entity['content'] = value
+        app.create_node(entity)
+
     app.close()
